1379c2460065a27c61c0eaf7f1faefa36682a80d
Alyona <polova_alyona0907@ukr.net>
2023-06-13 20:38:32 +0200

    5hw

---
+++ hw5.js
@@ -0,0 +1,15 @@
+const parent = {
+  name: "Stacey",
+  surname: "Moore",
+  age: 54,
+  heritage: "Irish",
+};
+// Change code below this line
+
+const child = Object.create{parent};
+
+// Change code above this line
+child.name = "Jason";
+child.age = 27;
+
+console.log(child)
